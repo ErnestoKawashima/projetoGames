6ab0e4bc3ea65ea6505ba7ef3127e81aec552d2b
Ernesto Kawashima <ernesto.kawashima@gmail.com>
2023-07-15 16:35:28 -0300

    Game DTO, repository, service, controller

---
+++ src/main/java/com/projeto/dslist/dto/GameMinDTO.java
@@ -0,0 +1,38 @@
+package com.projeto.dslist.dto;
+
+public class GameMinDTO {
+
+    private long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO(long id, String title, Integer year, String imgUrl, String shortDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
---
+++ src/main/java/com/projeto/dslist/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package com.projeto.dslist.repositories;
+
+import entitites.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class GameRepository extends JpaRepository<Game, Long> {
+}
