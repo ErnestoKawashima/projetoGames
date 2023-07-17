a68ca7e093a03a5ef2460f9346fa3f9972869ac6
Ernesto Kawashima <ernesto.kawashima@gmail.com>
2023-07-17 18:07:37 -0300

    Encontrando todas as listas

--- src/main/java/com/projeto/dslist/services/GameService.java
+++ src/main/java/com/projeto/dslist/services/GameService.java
@@ -1,10 +1,13 @@
 package com.projeto.dslist.services;
 
+import com.projeto.dslist.dto.GameDTO;
 import com.projeto.dslist.dto.GameMinDTO;
 import com.projeto.dslist.entities.Game;
 import com.projeto.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -13,6 +16,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(@PathVariable long listId) {
+        Game result = gameRepository.findById(listId).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDTO::new).toList();
